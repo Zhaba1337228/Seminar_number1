6b3d0315f5c9abb6f8b0c4ae15268ab740089f5d
Zhaba1337228 <157302440+Zhaba1337228@users.noreply.github.com>
2025-09-09 11:28:37 +0300

    Update 1_task.cpp

--- 1_task.cpp
+++ 1_task.cpp
@@ -4,7 +4,9 @@
 int main()
 {
     long long A,B;
+    std::cout << "enter 2 numbers separated by a space to get the output amount: ";
     std::cin >> A >> B;
+    std::cout << "amount:";
     std::cout << A + B;
     return 0;
 }

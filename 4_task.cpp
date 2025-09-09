956a1d79b65232b2a0f3cab6eb5687f99a094800
Zhaba1337228 <157302440+Zhaba1337228@users.noreply.github.com>
2025-09-09 11:32:08 +0300

    Update 4_task.cpp

--- 4_task.cpp
+++ 4_task.cpp
@@ -4,6 +4,7 @@
 int main()
 {
     long long v,t;
+    std::cout << "enter the speed and time separated by a space: ";
     std::cin >> v >> t;
     long long s = (v * t) % 109;
     std::cout << s;

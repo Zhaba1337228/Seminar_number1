d0b8ac1cb68e02b130e8e080ad1ab18b73d8c027
Zhaba1337228 <157302440+Zhaba1337228@users.noreply.github.com>
2025-09-09 11:30:02 +0300

    Update 2_task.cpp

--- 2_task.cpp
+++ 2_task.cpp
@@ -4,6 +4,7 @@
 int main()
 {
     long long A,B;
+    std::cout << "enter 2 numbers separated by a space: ";
     std::cin >> A >> B;
     std::cout << A + B << " ";
     std::cout << A - B << " ";
